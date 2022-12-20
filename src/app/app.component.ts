f4c413c835bbcc5abd7d7e0227ea656fd9a13030
phanphuc2210 <phucpth2001@gmail.com>
2022-12-20 07:32:56 +0700

    Create Basic Angular animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
+import { trigger, transition, style, animate } from '@angular/animations'
+
+const enterTransition = transition(':enter', [
+  style({
+    opacity: 0,
+  }),
+  animate('1s ease-in', style({opacity: 1})),
+]);
+const exitTransition = transition(':leave', [
+  style({
+    opacity: 1,
+  }),
+  animate('1s ease-out', style({opacity: 0})),
+])
+
+const fadeIn = trigger('fadeIn', [enterTransition]);
+const fadeOut = trigger('fadeOut', [exitTransition]);
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [fadeIn, fadeOut]
 })
 export class AppComponent {
   title = 'Animation_NgZone_Angular';
+
+  isShow = false;
+
+  public fadeInOut():void {
+    this.isShow = !this.isShow;
+  }
 }
