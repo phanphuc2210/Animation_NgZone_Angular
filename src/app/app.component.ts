de0afcbc64f15a92ff0f3e8a17524b0705ef22dc
phanphuc2210 <phucpth2001@gmail.com>
2022-12-20 07:44:19 +0700

    States in Angular animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,46 @@
 import { Component } from '@angular/core';
-import { trigger, transition, style, animate } from '@angular/animations'
+import { trigger, transition, style, animate, state } from '@angular/animations'
 
-const enterTransition = transition(':enter', [
-  style({
-    opacity: 0,
-  }),
-  animate('1s ease-in', style({opacity: 1})),
-]);
-const exitTransition = transition(':leave', [
-  style({
-    opacity: 1,
-  }),
-  animate('1s ease-out', style({opacity: 0})),
+// const enterTransition = transition(':enter', [
+//   style({
+//     opacity: 0,
+//   }),
+//   animate('1s ease-in', style({opacity: 1})),
+// ]);
+// const exitTransition = transition(':leave', [
+//   style({
+//     opacity: 1,
+//   }),
+//   animate('1s ease-out', style({opacity: 0})),
+// ])
+
+// const fadeIn = trigger('fadeIn', [enterTransition]);
+// const fadeOut = trigger('fadeOut', [exitTransition]);
+
+// v2
+const fadeInOut = trigger('fadeInOut', [
+  state(
+    'open',
+    style({
+      opacity: 1
+    })
+  ),
+  state(
+    'close',
+    style({
+      opacity: 0
+    })
+  ),
+  transition('open => close', [animate('1s ease-out')]),
+  transition('close => open', [animate('1s ease-in')]),
 ])
-
-const fadeIn = trigger('fadeIn', [enterTransition]);
-const fadeOut = trigger('fadeOut', [exitTransition]);
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  animations: [fadeIn, fadeOut]
+  // animations: [fadeIn, fadeOut],
+  animations: [fadeInOut]
 })
 export class AppComponent {
   title = 'Animation_NgZone_Angular';
